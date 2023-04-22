eee20407d067f8f55dfa9d6ee7a4b4f28d0506a7
mohammed-khalif <mohamed.yusuf@student.moringaschool.com>
2023-04-22 15:22:24 -0400

    completed the assignment

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,15 @@ import blogData from "../data/blog";
 console.log(blogData);
 
 function App() {
+  const name = blogData.name
+  const about = blogData.about
+  const image = blogData.image
   return (
     <div className="App">
+       <Header name={name} />
+       <About about={about} image={image}/>
+       <ArticleList/>
+      
       You're on your own from here! Follow the deliverables; test things out in
       the browser as you write your code; and good luck!
     </div>
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = (props) => {
+
+    return (
+        <header>
+            <h1>{name}</h1>
+        </header>
+    );
+}
+
+export default Header;
